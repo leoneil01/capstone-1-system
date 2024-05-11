96c7f584fb0173db1db1041f3ce1439ac06c0e88
izme170 <jericolopez2003@gmail.com>
2024-05-12 01:29:06 +0800

    Landing Page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.main')
+@section('content')
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="card" style="max-width: 740px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <div class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="images/carousel/1.png" class="d-block w-100 img-fluid rounded-start" alt="..."
+                                    draggable="false">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Inventory Management</h5>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/carousel/2.png" class="d-block w-100 img-fluid rounded-start"
+                                    alt="..." draggable="false">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Customizable Reporting</h5>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/carousel/3.png" class="d-block w-100 img-fluid rounded-start"
+                                    alt="..." draggable="false">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>User-Friendly Interface</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-end pb-4">
+                    <div class="card-body">
+                        <h1 class="card-title">Laravel POS System</h1>
+                        <p class="card-text">Please select your role.</p>
+                        <div class="d-flex flex-row flex-wrap justify-content-center">
+                            <button>Admin</button>
+                            <button>Cashier</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
