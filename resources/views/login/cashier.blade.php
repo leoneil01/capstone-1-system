f91703af0ecccc2d0fa252bb0430f8b182d4411b
izme170 <jericolopez2003@gmail.com>
2024-05-12 23:08:55 +0800

    Added two Modal Login Form for cashier and admin

---
+++ resources/views/login/cashier.blade.php
@@ -0,0 +1,26 @@
+<div class="modal fade" id="cashierLoginModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="cashierLoginModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-dark text-white">
+            <div class="modal-header border-bottom-0">
+                <h1 class="modal-title fs-5" id="cashierLoginModalLabel">Cashier Login</h1>
+            </div>
+            <form>
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Username</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="username">
+                    </div>
+                </div>
+                <div class="modal-footer border-top-0">
+                    <button type="button" class="btn-cancel" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
