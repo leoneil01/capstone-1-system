07faea1e2055693ece2f0050b6ee242cf15a7879
izme170 <jericolopez2003@gmail.com>
2024-05-13 00:54:13 +0800

    Added Icons to buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 @extends('login.cashier')
 @section('content')
     <div class="position-absolute top-50 start-50 translate-middle">
-        <div class="card border-0 bg-dark-alpha text-white" style="max-width: 740px;">
+        <div class="card border-0 bg-dark-alpha text-white card-responsive" style="max-width: 900px;">
             <div class="row g-0">
-                <div class="col-md-6">
+                <div class="col-lg-6">
                     <div class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
@@ -32,13 +32,13 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 d-flex align-items-center">
+                <div class="col-lg-6 d-flex align-items-center">
                     <div class="card-body">
                         <h1 class="card-title">Laravel POS System</h1>
                         <p class="card-text">Please select your role.</p>
                         <div class="d-flex flex-row flex-wrap justify-content-center">
-                            <button data-bs-toggle="modal" data-bs-target="#adminLoginModal">Admin</button>
-                            <button data-bs-toggle="modal" data-bs-target="#cashierLoginModal">Cashier</button>
+                            <button data-bs-toggle="modal" data-bs-target="#adminLoginModal"><x-fas-user-tie class="fas-icon"/>Admin</button>
+                            <button data-bs-toggle="modal" data-bs-target="#cashierLoginModal"><x-fas-cash-register class="fas-icon" />Cashier</button>
                         </div>
                     </div>
                 </div>
