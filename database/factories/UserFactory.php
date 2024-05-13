e321d602fd7f08c6fb65d7ccec6b17967fe4235e
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2024-05-13 23:06:25 +0800

    add model, controller, migration, factory for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'first_name' => fake()->firstName(),
+            'middle_name' => fake()->lastName(),
+            'last_name' => fake()->lastName(),
+            'gender_id' => fake()->numberBetween($min = 1, $max = 2),
+            'role_id' => fake()->numberBetween($min = 1, $max = 2),
+            'address' => fake()->streetAddress(),
+            'birth_date' => fake()->date(),
+            'email_address' => fake()->safeEmail(),
+            'username' => fake()->userName(),
+            'password' => bcrypt('123')
         ];
     }
 
