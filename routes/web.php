d84ff38149598a56ebe0abc15037857615bffa19
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2024-05-21 07:42:09 +0800

    add cashier routes

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,28 @@ use Illuminate\Support\Facades\Auth;
 
 Route::controller(UserController::class)->group(function() {
     Route::get('/', 'login')->name('login');
-    Route::post('/process/login', function(Request $request) {
+    Route::post('/process/login/admin', function(Request $request) {
         return app()->call('App\Http\Controllers\UserController@processLogin', ['request' => $request, 'role' => 1]);
+    });
+    Route::post('/process/login/cashier', function(Request $request) {
+        return app()->call('App\Http\Controllers\UserController@processLogin', ['request' => $request, 'role' => 2]);
     });
 });
 
 Route::group(['middleware' => 'auth'], function() {
     Route::controller(UserController::class)->group(function() {
         Route::get('/admin', 'admin.index');
+        Route::get('/cashier', 'admin.index');
     });
 
     Route::get('/admin', function () {
+        $firstName = Auth::user()->first_name;
+
+        Alert::toast('Welcome, ' . $firstName . '!', 'Toast Type');
+        return view('admin.index');
+    });
+
+    Route::get('/cashier', function () {
         $firstName = Auth::user()->first_name;
 
         Alert::toast('Welcome, ' . $firstName . '!', 'Toast Type');
