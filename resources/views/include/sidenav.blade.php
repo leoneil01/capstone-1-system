8a4b29802eecbc32f42b91bfb14ce8e2e619831d
izme170 <jericolopez2003@gmail.com>
2024-05-22 13:13:14 +0800

    Set href of links to #. Change the User image and name to logo and logo title

--- resources/views/include/sidenav.blade.php
+++ resources/views/include/sidenav.blade.php
@@ -1,39 +1,41 @@
 <div class="sidenav">
     <div class="sidenav-header">
+        <img src="{{ asset('images/temporary_logo.png') }}" alt="Logo" class="logo" draggable="false">
+        <h3 class="logo-title">ShopNinja</h3>
     </div>
     <ul>
         <li>
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-home class="fas-icon" /></span>
                 <span class="sidenav-title">Dashboard</span>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-boxes class="fas-icon" /></span>
                 <span class="sidenav-title">Products</span>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-user-alt class="fas-icon" /></span>
                 <span class="sidenav-title">Users</span>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-truck-ramp-box class="fas-icon" /></span>
                 <span class="sidenav-title">Supplier</span>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-receipt class="fas-icon" /></span>
                 <span class="sidenav-title">Transactions</span>
             </a>
         </li>
         <li class="sign-out-link">
-            <a href="">
+            <a href="#">
                 <span class="sidenav-icon"><x-fas-sign-out-alt class="fas-icon" /></span>
                 <span class="sidenav-title">Sign out</span>
             </a>
