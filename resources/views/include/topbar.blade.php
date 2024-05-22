406aa7b01da03a7a07755d164e028920da11a2d8
izme170 <jericolopez2003@gmail.com>
2024-05-22 13:14:21 +0800

    Add topbar with sidenav toggle button, user image and fullname

---
+++ resources/views/include/topbar.blade.php
@@ -0,0 +1,9 @@
+<div class="topbar">
+    <div class="toggle">
+        <x-fas-list class="fas-icon"/>
+    </div>
+    <div class="user">
+        <span class="fullname">Full Name</span>
+        <img src="{{ asset('images/default_profile_image.jpg') }}" alt="User Image" class="user-img" draggable="false">
+    </div>
+</div>
