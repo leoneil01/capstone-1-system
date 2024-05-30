be672621e73e2e44e3605a1c3325653822521686
izme170 <jericolopez2003@gmail.com>
2024-05-30 23:37:36 +0800

    Link all designs and js

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/sidenav.css')}}">
+    <link rel="stylesheet" href="{{asset('css/topbar.css')}}">
+    <link rel="stylesheet" href="{{asset('css/cards.css')}}">
+    <link rel="stylesheet" href="{{asset('css/buttons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/table.css')}}">
 </head>
 <body>
 
@@ -17,8 +22,8 @@
         @yield('login')
     </div>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/script.js"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('js/script.js')}}"></script>
 
     @include('sweetalert::alert')
 </body>
