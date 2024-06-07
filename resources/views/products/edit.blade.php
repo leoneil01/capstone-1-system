cf02241330ba579da4a3cb655613bfb080c153ec
izme170 <jericolopez2003@gmail.com>
2024-06-07 21:46:13 +0800

    Update Edit page

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -2,25 +2,52 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editProductLabel">View Product</h5>
+                <h5 class="modal-title" id="editProductLabel">Edit Product</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="">
-                    <div><img src="{{ asset('images/sample_image.jpg') }}" id="image" alt="Product Image"></div>
-                    <div><label for="new-image">Upload Image:</label><input id="new-image" type="file"></div>
-                    <div><label for="product-name">Product Name:</label><input type="text" id="product-name"
-                            value="Milo"></div>
-                    <div><label for="supplier">Supplier:</label><input type="text" id="supplier" value="Nestle">
+                    <div>
+                        <img src="{{ asset('images/sample_image.jpg') }}" id="image" alt="Product Image">
                     </div>
-                    <div><label for="category">Category:</label><input type="text" id="category"
-                            value="Powedered Milk">
+                    <div>
+                        <label for="new-image">Upload Image:</label>
+                        <input id="new-image" type="file">
                     </div>
-                    <div><label for="brand">Brand:</label><input type="text" id="brand" value="Nestle"></div>
-                    <div><label for="barcode">Barcode:</label><input type="text" id="barcode" value="12121212121">
+                    <div>
+                        <label for="product-name">Product Name:</label>
+                        <input type="text" id="product-name" value="Milo">
                     </div>
-                    <div><label for="price">Price:</label><input type="text" id="price" value="12.00"></div>
-                    <div><label for="stock">Stock:</label><input type="text" id="stock" value="500"></div>
+                    <div>
+                        <label for="supplier">Supplier:</label>
+                        <select name="supplier" id="supplier">
+                            <option value="1" selected>S1</option>
+                            <option value="2">S2</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="category">Category:</label>
+                        <select name="category" id="category">
+                            <option value="1" selected>c1</option>
+                            <option value="2">c2</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="brand">Brand:</label>
+                        <input type="text" id="brand" value="Nestle">
+                    </div>
+                    <div>
+                        <label for="barcode">Barcode:</label>
+                        <input type="text" id="barcode" value="12121212121">
+                    </div>
+                    <div>
+                        <label for="price">Price:</label>
+                        <input type="text" id="price" value="12.00">
+                    </div>
+                    <div>
+                        <label for="stock">Stock:</label>
+                        <input type="text" id="stock" value="500">
+                    </div>
                     <button class="btn-simple">Save</button>
                 </form>
             </div>
