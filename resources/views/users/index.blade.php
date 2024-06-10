f0c715b466ba91e643dae4a2a698b09b41e6fa09
izme170 <jericolopez2003@gmail.com>
2024-06-10 23:59:22 +0800

    Modify Table

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,24 +17,16 @@
             <thead class="thead-dark">
                 <tr>
                     <th scope="col">Fullname</th>
-                    <th scope="col">Gender</th>
                     <th scope="col">Role</th>
-                    <th scope="col">Address</th>
-                    <th scope="col">Birthdate</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Username</th>
                     <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Juan Dela Cruz</td>
-                    <td>Male</td>
                     <td>Admin</td>
-                    <td>Roxas City</td>
-                    <td>01/01/2024</td>
                     <td>juan@gmail.com</td>
-                    <td>juan</td>
                     <td>
                         <div class="btn-group" role="group" aria-label="Basic example">
                             <button class="btn btn-outline-primary" data-bs-toggle="modal"
